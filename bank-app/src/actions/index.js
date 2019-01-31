8ca6e511d2db37067064ba39dd5945fedacb4f03
acrosier <acrosier@cameo-innovation.com>
2019-01-31 17:52:37 +0100

    Delete bank-app from time-counter branch

--- bank-app/src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export const setWithdraw = (balance) => {
-  console.log(balance);
-  return { type: "WITHDRAW", balance: balance}
-}
--- bank-app/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-
-import reducer from '../reducers';
-
-const initialState = { name: "Janny", balance: "102500.701"}
-export const store = createStore(reducer, initialState);
